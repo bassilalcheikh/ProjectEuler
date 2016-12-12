95cdb767142e37870a5860fc3c966fe8e74d941d
bassilalcheikh <bassil.alcheikh@gmail.com>
2016-12-12 16:47:41 -0600

    Added problem #24

---
+++ ProjEulerP24.py
@@ -0,0 +1,21 @@
+# Project Euler Problem 24
+#
+# What is the millionth lexicographic permutation of the digits 0-9?
+#
+from math import factorial, floor
+
+t_0 = time()
+candidates = [i for i in range(0,10)]
+perm_elem = []
+index = 10**6-1
+
+for j in range(9,-1,-1):
+    n = index/factorial(j)
+    perm_elem.append(str(candidates[n]))
+    index = index - n*factorial(j)
+    del candidates[n]
+
+solution = ''.join(perm_elem)
+
+# times:
+# (1) 0.0 seconds
