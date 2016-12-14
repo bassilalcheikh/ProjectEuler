6855fcfa93c7ac3f550f525f2d6ddd55440f7c46
bassilalcheikh <bassil.alcheikh@gmail.com>
2016-12-14 17:11:43 -0600

    Update ProjEulerP19.py

--- ProjEulerP19.py
+++ ProjEulerP19.py
@@ -24,3 +24,18 @@ for i in range(5,days_count,7):
     days_index[i] += 1
 
 print(sum(days_index))
+
+# Month| STD | Leap
+# -----|-----|-----
+# Jan  |   1 |   1
+# Feb  |  32 |  32
+# Mar  |  60 |  61
+# Apr  |  91 |  92
+# May  | 121 | 122
+# Jun  | 152 | 153
+# Jul  | 182 | 183
+# Aug  | 213 | 214
+# Sep  | 244 | 245
+# Oct  | 274 | 275
+# Nov  | 305 | 306
+# Dec  | 335 | 336
